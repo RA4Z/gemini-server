dd9ec99d5077e8a1cdb7d81ec74bbfa0005bf56e
Robert Aron Zimmermann <robertn@weg.net>
2024-06-10 14:14:41 -0300

    Adicionando dados de transações

--- data_chatbot.py
+++ data_chatbot.py
@@ -13,6 +13,7 @@ path_procedures = '\\\\intranet.weg.net@SSL\\DavWWWRoot\\br\\energia-wm\\pcp\\Ce
 default_answers = json.load(open('Q:\\GROUPS\\BR_SC_JGS_WM_LOGISTICA\\PCP\\PPC_AI_Procedures\\documents\\default_answers.json', 'r', encoding='utf-8'))
 pathsPCP = json.load(open('Q:\\GROUPS\\BR_SC_JGS_WM_LOGISTICA\\PCP\\PPC_AI_Procedures\\documents\\Links_Uteis.json', 'r', encoding='utf-8'))
 agendaPCP = json.load(open('Q:\\GROUPS\\BR_SC_JGS_WM_LOGISTICA\\PCP\\PPC_AI_Procedures\\documents\\agenda.json', 'r', encoding='utf-8'))
+transacoesWEG = json.load(open('Q:\\GROUPS\\BR_SC_JGS_WM_LOGISTICA\\PCP\\PPC_AI_Procedures\\documents\\TRANSAÇÕES-WEG.json', 'r', encoding='utf-8'))
 
 paths = [path_daily, path_weekly, path_monthly, path_procedures]
 historico = []
@@ -29,6 +30,20 @@ for path in pathsPCP:
         "role": "model",
         "parts": [
             f"Link da Web: {path['web_path']}\nPasta da Rede: {path['groups_path']}"
+        ]
+    })
+    
+for transacao in transacoesWEG:
+    historico.append({
+        "role": "user",
+        "parts": [
+            f"Transação {transacao['Transação']}"
+        ]
+    })
+    historico.append({
+        "role": "model",
+        "parts": [
+            f"{transacao['Transação']}: {transacao['Título da Transação']}"
         ]
     })
 
