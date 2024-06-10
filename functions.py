67109de9e1c4567e3e23a610183c9ce3ae9cac7d
RA4Z <rraz639@gmail.com>
2024-06-10 15:05:51 -0300

    Não salvando perguntas de Robert

--- functions.py
+++ functions.py
@@ -10,10 +10,11 @@ def ultimo_dia_util(data):
 
 
 def input_history(name: str, text_input: str, output: str):
-    indicadores = json.load(
-        open('Q:/GROUPS/BR_SC_JGS_WM_LOGISTICA/PCP/PPC_AI_Procedures/documents/inputs_history.json', 'r',
-             encoding='utf-8'))
-    indicadores.append({"username": name, "input": text_input, "output": output, "time": str(datetime.datetime.now())})
-    json.dump(indicadores,
-              open('Q:/GROUPS/BR_SC_JGS_WM_LOGISTICA/PCP/PPC_AI_Procedures/documents/inputs_history.json', 'w',
-                   encoding='utf-8'), indent=4)
+    if name != 'robertn':
+        indicadores = json.load(
+            open('Q:/GROUPS/BR_SC_JGS_WM_LOGISTICA/PCP/PPC_AI_Procedures/documents/inputs_history.json', 'r',
+                 encoding='utf-8'))
+        indicadores.append({"username": name, "input": text_input, "output": output, "time": str(datetime.datetime.now())})
+        json.dump(indicadores,
+                  open('Q:/GROUPS/BR_SC_JGS_WM_LOGISTICA/PCP/PPC_AI_Procedures/documents/inputs_history.json', 'w',
+                       encoding='utf-8'), indent=4)
